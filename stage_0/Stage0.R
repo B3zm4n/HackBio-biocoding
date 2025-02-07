e9a78e105ef9c877f81fe45f4acb7c5df663be48
B3zm4n <126209580+B3zm4n@users.noreply.github.com>
2025-02-07 17:27:35 -0500

    Add files via upload

---
+++ stage_0/Stage0.R
@@ -0,0 +1,11 @@
+first <- c('Bezaleel', 'Kazadi', 'Hana', 'Onyinye', 'Sanzida')
+last <- c('Akinbami', 'Ngole', 'Nadir', 'Maryrose_Ugwu', 'Akhter_Anee')
+slack_name <- c('@Bez', '@Tatiene', '@Hana', '@Onyinye', '@Sanzida')
+email <- c('bezaleelakinbami16@gmail.com', 'tatianagoie17@gmail.com', 'hananadir04@gmail.com', 'moyinye80@gmail.com', 'aneesanzidaakhter@gmail.com')
+hobby <- c('gaming', 'gaming', 'Reading', 'Traveling', 'Blogging')
+country <- c('Nigeria', 'South Africa', 'Sudan', 'Nigeria', 'Sudan')
+discipline <- c('Bioinformatics', 'Bioinformatics', 'Medicinal chemistry', 'Zoology', 'Bioinformatics')
+program_lang <- c('R', 'Python', 'Python', 'Python', 'Python')
+
+
+data0 <- data.frame(First = first, Last = last, Slack_Name = slack_name, Email = email, Hobby = hobby, Country = country, Discipline = discipline, Program_Lang = program_lang)
